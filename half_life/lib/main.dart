fc2f36f12c76731b569297e89c3b265f7d359443
Bryan Cancel <bryan.o.cancel@gmail.com>
2020-02-28 04:13:41 -0600

    super basic function is working, functional for now expect I need to add doses manually ofcourse

--- half_life/lib/main.dart
+++ half_life/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:half_life/struct/doses.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,21 +25,113 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  DateTime now;
+  Duration halfLife = Duration(hours: 36);
+  List<Dose> doses = new List<Dose>();
+  int maxActiveDose = 40;
 
   @override
   void initState() {
-    // TODO: implement initState
+    now = DateTime.now();
+
+    doses.add(Dose(100, DateTime(
+      2020,
+      2, //feb
+      20, //20
+      1 //1 am
+    )));
+
+    doses.add(Dose(50, DateTime(
+      2020,
+      2, //feb
+      21, //21
+      1 //1 am
+    )));
+
+    doses.add(Dose(25, DateTime(
+      2020,
+      2,
+      28,
+      4,
+    )));
+
     super.initState();
+  }
+
+  double sliderValue = 1;
+  int dosesBeingConsidered;
+  DateTime firstDose;
+  Duration elapsedTime;
+  DateTime lastDateTime;
+
+  calcActiveDose(){
+    firstDose = doses[0].timeStamp;
+    Duration timeSinceFirst = DateTime.now().difference(firstDose);
+    elapsedTime = Duration(
+      microseconds: (timeSinceFirst.inMicroseconds * sliderValue).round(),
+    );
+    lastDateTime = firstDose.add(elapsedTime);
+    
+    //loop through doses to calculate 
+    //how much we have left of each after the selected time
+    double totalDose = 0;
+    dosesBeingConsidered = 0;
+    for(int i = 0; i < doses.length; i++){
+      bool doseBefore = doses[i].timeStamp.isBefore(lastDateTime);
+      bool doseAt = doses[i].timeStamp.difference(lastDateTime) == Duration.zero;
+      if(doseBefore || doseAt){
+        dosesBeingConsidered++;
+
+        int dose = doses[i].value;
+        DateTime timeTaken = doses[i].timeStamp;
+        Duration timeSinceTaken = lastDateTime.difference(timeTaken);
+
+        //allways use duration in microseconds
+        double decayConstant = math.log(2) / halfLife.inMicroseconds;
+        double exponent = -decayConstant * timeSinceTaken.inMicroseconds;
+        double dosageLeft = dose * math.pow(math.e, exponent);
+
+        totalDose += dosageLeft;
+      }
+      //ELSE: we don't count it
+    }
+
+    return totalDose;
   }
 
   @override
   Widget build(BuildContext context) {
+    double activeDose = calcActiveDose();
+    double maxDose = maxActiveDose - activeDose;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text("center"),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Your first dose was on: " + firstDose.toString()),
+            Text("and after: " + elapsedTime.toString()),
+            Text("on: " + lastDateTime.toString()),
+            Text("you had only taken: " + dosesBeingConsidered.toString() + " doses"),
+            Text("and your active dose was: " + activeDose.toString()),
+            Text("Max Active Dose: " + maxActiveDose.toString()),
+            Text("Max Dose To Take To Stay Below Max Active Dose"),
+            Text(maxDose.toString()),
+            Slider(
+              min: 0,
+              max: 1,
+              value: sliderValue, 
+              onChanged: (double newValue) {
+                setState(() {
+                  sliderValue = newValue;
+                });
+              },
+            ),
+          ],
+        )
       ),
     );
   }
